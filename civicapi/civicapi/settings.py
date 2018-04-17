fa73fde5fc4addcfd42eee3b7df2ac8d46edf177
Philip James <pjj@philipjohnjames.com>
2018-04-16 22:53:08 -0700

    Add a simple frontend to the API

--- civicapi/civicapi/settings.py
+++ civicapi/civicapi/settings.py
@@ -82,6 +82,11 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
--- civicapi/votes/views.py
+++ civicapi/votes/views.py
@@ -1,14 +1,38 @@
+from django.shortcuts import redirect
 from rest_framework import generics
+from rest_framework.renderers import (
+    BrowsableAPIRenderer,
+    JSONRenderer,
+    TemplateHTMLRenderer,
+)
 
 from .models import Vote
 from .serializers import VoteSerializer
 
 
 class VoteList(generics.ListCreateAPIView):
+    renderer_classes = (
+        TemplateHTMLRenderer,
+        JSONRenderer,
+        BrowsableAPIRenderer,
+    )
+    template_name = "vote_list.html"
     queryset = Vote.objects.all()
     serializer_class = VoteSerializer
 
+    def create(self, request, *args, **kwargs):
+        response = super(VoteList, self).create(request, *args, **kwargs)
+        if request.accepted_media_type == 'text/html' and response.status_code == 201:
+            return redirect('/votes/')
+        return response
+
 
 class VoteDetail(generics.RetrieveUpdateDestroyAPIView):
+    renderer_classes = (
+        TemplateHTMLRenderer,
+        JSONRenderer,
+        BrowsableAPIRenderer,
+    )
+    template_name = "vote.html"
     queryset = Vote.objects.all()
     serializer_class = VoteSerializer
