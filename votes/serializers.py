23fddece6e2738590c79442a6c69d1a36d7ea871
Philip James <pjj@philipjohnjames.com>
2018-05-09 14:46:00 -0400

    Add DRF, vote serializer

---
+++ votes/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from .models import Vote
+
+
+class VoteSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Vote
+        fields = ('id', 'subject', 'vote_taken', 'ayes', 'nays')
--- votes/tests.py
+++ votes/tests.py
@@ -17,3 +17,20 @@ class VoteModelTests(TestCase):
             time.strftime('%c'),
         )
         assert expected_string == str(vote)
+
+
+class VoteSerializerTests(TestCase):
+
+    def test_serialization(self):
+        time = timezone.now()
+        vote = Vote.objects.create(
+            subject="More projects built in Django",
+            ayes=100,
+            nays=0,
+            vote_taken=time,
+        )
+        serialized = VoteSerializer(vote).data
+        assert vote.id == serialized['id']
+        assert vote.subject == serialized['subject']
+        assert vote.ayes == serialized['ayes']
+        assert vote.nays == serialized['nays']
