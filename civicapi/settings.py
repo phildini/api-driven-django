cf724d4260bbcf338728eb96d2ec68ff5714ab23
Philip James <pjj@philipjohnjames.com>
2018-05-09 15:54:47 -0400

    Add token authentication

--- civicapi/settings.py
+++ civicapi/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'votes',
 ]
 
@@ -74,6 +75,11 @@ WSGI_APPLICATION = 'civicapi.wsgi.application'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    )
 }
 
 
