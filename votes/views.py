974efbd629a5476dcc688c784e9e305b26c4422b
Philip James <pjj@philipjohnjames.com>
2018-05-09 15:11:43 -0400

    Add vote creation to template

--- votes/views.py
+++ votes/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import redirect
+
 from rest_framework import generics
 
 from rest_framework.renderers import (
@@ -19,3 +21,10 @@ class VoteList(generics.ListCreateAPIView):
     template_name = "vote_list.html"
     queryset = Vote.objects.all()
     serializer_class = VoteSerializer
+
+    def create(self, request, *args, **kwargs):
+        response = super(VoteList, self).create(request, *args, **kwargs)
+        if request.accepted_renderer.format == 'html' and response.status_code == 201:
+            return redirect('/votes/')
+        return response
+
